e7d39670a5c66065627f07e00d5fdaf655cd099a
Yash Chowdhary <yash.chowdhary@neosoftmail.com>
2021-04-23 11:28:50 +0530

    First Commit

---
+++ TestProject/src/com/prac/test/GitHubTest.java
@@ -0,0 +1,13 @@
+package com.prac.test;
+
+public class GitHubTest {
+public static void main(String Args[]) {
+	String val="SE";
+	if(val.equalsIgnoreCase("SE")) {
+		System.out.println("You are an Software Engineer");
+	}
+	else {
+		System.out.println("You are not an Software Engineer");
+	}
+}
+}
